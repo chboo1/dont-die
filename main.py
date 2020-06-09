6fee6d43c11a789d7305defb7f8912b70a80060f
Samuel Navert <samuel.navert@gmail.com>
2020-06-09 15:48:26 -0400

    Update

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from random import randint
 
 class Main():
     def __init__(self):
+        self.youhavewon = False
         self.root = Tk()
         self.death = False
         self.camson = False
+        self.rdoor_closed = False
+        self.ldoor_closed = False
         self.left = True
         self.width = self.root.winfo_screenwidth()
         self.height = self.root.winfo_screenheight()
@@ -17,16 +20,25 @@ class Main():
         self.c.pack()
         self.bowlybodyB = PhotoImage(file="bowly_chains.png")
         self.bowlyheadB = PhotoImage(file="bowly_head.png")
-        self.c.create_rectangle(0, 0, self.width, self.height, fill="#606060",
-                                outline="#606060")
+        self.background = self.c.create_rectangle(0, 0, self.width,
+                                                  self.height, fill="#606060",
+                                                  outline="#606060")
+        self.ldoor = self.c.create_polygon(30, 100, 230, 150, 230,
+                                           self.height - 150,
+                                           30, self.height - 100,
+                                           fill="#000000")
+        self.rdoor = self.c.create_polygon(self.width - 30, 100,
+                                           self.width - 230, 150,
+                                           self.width - 230, self.height - 150,
+                                           self.width - 30, self.height - 100,
+                                           fill="#000000")
         self.bowlyhead = self.c.create_image(self.width / 2, self.height / 2,
                                              anchor="c", image=self.bowlyheadB,
                                              state="hidden")
         self.wd = self.width / 1920
         self.hd = self.width / 1080
         self.camm = PhotoImage(file="cammap.png")
-        self.text = self.c.create_text(0, 0, anchor="nw", text="")
-        self.text2 = self.c.create_text(0, 15, anchor="nw", text="")
+        self.text = self.c.create_text(15, 15, anchor="nw", text=" 12 PM ")
         self.cama = PhotoImage(file="camA.png")
         self.bowlycama = PhotoImage(file="camAbowly.png")
         self.currentcam = self.c.create_image(0, 0, anchor="nw",
@@ -42,10 +54,30 @@ class Main():
         self.incama = False
         self.c.tag_bind(self.camicon, "<Leave>", self.cams_off)
         self.root.bind("<Motion>", self.motion)
-        self.root.bind("d", self.die)
+        self.root.bind("a", self.closeleft)
+        self.root.bind("d", self.closeright)
+        self.root.bind("w", self.WIN)
         self.root.bind("<Escape>", self.kr)
         self.root.after(10, self.eachcsec)
+        self.root.after(3*60*1000, self.WIN)
         self.root.mainloop()
+
+    def closeleft(self, event=None):
+        if not self.ldoor_closed:
+            self.ldoor_closed = True
+            self.c.itemconfig(self.ldoor, fill="#505050")
+        else:
+            self.ldoor_closed = False
+            self.c.itemconfig(self.ldoor, fill="#000000")
+
+    def closeright(self, event=None):
+        if not self.rdoor_closed:
+            self.rdoor_closed = True
+            self.c.itemconfig(self.rdoor, fill="#505050")
+
+        else:
+            self.rdoor_closed = False
+            self.c.itemconfig(self.rdoor, fill="#000000")
 
     def die(self, event=""):
         self.death = True
@@ -69,16 +101,20 @@ class Main():
         self.bowlyattack = randint(0, 500)
         if self.bowlyattack == 5:
             self.c.itemconfig(self.currentcam, image=self.bowlycama)
-            self.c.itemconfig(self.text, text=self.bowlyattack)
             self.incama = True
         if self.incama:
-            self.bowlyattack2 = randint(0, 999)
-        if (self.incama and self.bowlyattack2 == 5) or self.death:
+            self.bowlyattack2 = randint(0, 750)
+        if (self.incama and self.bowlyattack2 == 5 and not self.rdoor_closed)\
+                or self.death:
             self.c.itemconfig(self.currentcam, state="hidden")
             self.c.itemconfig(self.cammap, state="hidden")
             self.c.itemconfig(self.bowlyhead, state="normal")
-            self.root.after(5000, self.game_over_screen)
-        self.root.after(10, self.eachcsec)
+            self.root.after(3000, self.game_over_screen)
+        elif self.rdoor_closed and self.bowlyattack2 == 5:
+            self.incama = False
+            self.c.itemconfig(self.currentcam, image=self.cama)
+        if not self.youhavewon:
+            self.root.after(10, self.eachcsec)
 
     def cams_off(self, event=None):
         self.left = True
@@ -92,7 +128,21 @@ class Main():
                                 fill="red", outline="red")
         self.c.create_text(self.width / 2, self.height / 2, text="Game Over",
                            anchor="c", fill="#000000", font=("Helvetica", 100))
-        self.root.after(2000, self.kr)
+        self.root.after(500, self.kr)
+
+    def WIN(self, event=None):
+        self.youhavewon = True
+        self.c.create_rectangle(0, 0, self.width, self.height, fill="#000000")
+        self.c.tag_unbind(self.camicon, "<Enter>")
+        self.c.tag_unbind(self.camicon, "<Leave>")
+        self.c.create_text(self.width / 2, self.height / 2, anchor="c",
+                           fill="#ffffff", text="24 PM",
+                           font=("Helvetica", 500))
+        self.root.after(10000, self.kr)
+
+        def hour_add(self):
+            self.hour += 1
+            self.c.itemconfig()
 
 
 game = Main()
