f43ae11d1001bbbef17acbf49cab6aa4877954c5
Samuel Navert <samuel.navert@gmail.com>
2020-06-10 16:26:32 -0400

    pp

--- main.py
+++ main.py
@@ -4,26 +4,39 @@ from random import randint
 
 class Main():
     def __init__(self):
-        self.youhavewon = False
-        self.sec = 0
-        self.min = 0
-        self.hour = 0
+        # Screen, Canvas, etc...
         self.root = Tk()
-        self.death = False
-        self.camson = False
-        self.rdoor_closed = False
-        self.ldoor_closed = False
-        self.left = True
-        self.width = self.root.winfo_screenwidth()
-        self.height = self.root.winfo_screenheight()
         self.c = Canvas(self.root, width=self.width, height=self.height)
         self.root.geometry("{0}x{1}".format(self.width, self.height))
         self.root.title("Don't die")
         self.bowlyattack2 = 0
         self.c.pack()
+        # initiation of variables
+        self.youhavewon = False
+        self.electricity = 100
+        self.electricity2 = 0
+        self.sec = 0
+        self.min = 0
+        self.hour = 0
+        self.usage = 0
+        self.death = False
+        self.camson = False
+        self.rdoor_closed = False
+        self.ldoor_closed = False
+        self.left = True
         self.hour = 12
+        # Usefull variables
+        self.width = self.root.winfo_screenwidth()
+        self.height = self.root.winfo_screenheight()
+        self.wd = self.width / 1920
+        self.hd = self.width / 1080
+        # Image references `yay'
         self.bowlybodyB = PhotoImage(file="bowly_chains.png")
+        # Unused line /\
+        #             |
+        #             |
         self.bowlyheadB = PhotoImage(file="bowly_head.png")
+        # Images, text, etc...
         self.background = self.c.create_rectangle(0, 0, self.width,
                                                   self.height, fill="#606060",
                                                   outline="#606060")
@@ -39,19 +52,19 @@ class Main():
         self.bowlyhead = self.c.create_image(self.width / 2, self.height / 2,
                                              anchor="c", image=self.bowlyheadB,
                                              state="hidden")
-        self.wd = self.width / 1920
-        self.hd = self.width / 1080
         self.camm = PhotoImage(file="cammap.png")
-        self.text = self.c.create_text(15, 15, anchor="nw", text="12:00")
         self.cama = PhotoImage(file="camA.png")
         self.bowlycama = PhotoImage(file="camAbowly.png")
         self.currentcam = self.c.create_image(0, 0, anchor="nw",
                                               image=self.cama, state="hidden")
+        self.electext = self.c.create_text(15, 45, anchor="nw", text="100%")
+        self.text = self.c.create_text(15, 15, anchor="nw", text="12:00")
         self.cammap = self.c.create_image(self.width - 200, self.height - 200,
                                           anchor="se", image=self.camm,
                                           state="hidden")
         self.icon = PhotoImage(file="camera_icon.png")
-        self.camicon = self.c.create_image(self.width - ( self.width / 6 ), self.height - self.height / 9,
+        self.camicon = self.c.create_image(self.width - (self.width / 6),
+                                           self.height - self.height / 9,
                                            anchor="se", image=self.icon,
                                            state="normal")
         self.c.tag_bind(self.camicon, "<Enter>", self.cams_on)
@@ -68,18 +81,22 @@ class Main():
         if not self.ldoor_closed:
             self.ldoor_closed = True
             self.c.itemconfig(self.ldoor, fill="#505050")
+            self.usage += 1
         else:
             self.ldoor_closed = False
             self.c.itemconfig(self.ldoor, fill="#000000")
+            self.usage -= 1
 
     def closeright(self, event=None):
         if not self.rdoor_closed:
             self.rdoor_closed = True
             self.c.itemconfig(self.rdoor, fill="#505050")
+            self.usage += 1
 
         else:
             self.rdoor_closed = False
             self.c.itemconfig(self.rdoor, fill="#000000")
+            self.usage -= 1
 
     def die(self, event=""):
         self.death = True
@@ -93,20 +110,27 @@ class Main():
             if not self.camson:
                 self.c.itemconfig(self.cammap, state="normal")
                 self.c.itemconfig(self.currentcam, state="normal")
+                self.usage += 1
                 self.camson = True
             else:
                 self.c.itemconfig(self.currentcam, state="hidden")
                 self.c.itemconfig(self.cammap, state="hidden")
+                self.usage -= 1
                 self.camson = False
 
     def eachcsec(self):
         self.sec += 1
+        self.electricity2 += 1
         if self.sec % 187 == 0:
             self.min += 15
         if self.min % 60 == 0:
             self.hour += int(self.min / 60)
             self.min = 0
-        self.c.itemconfig(self.text, text="{0}:{1}".format(self.hour, self.min))
+        self.c.itemconfig(self.text,
+                          text="{0}:{1}".format(self.hour, self.min))
+        self.c.itemconfig(self.electext,
+                          text="{0}%   :    {1} U".format(self.electricity,
+                                                          self.usage))
         self.bowlyattack = randint(0, 825)
         if self.bowlyattack == 5:
             self.c.itemconfig(self.currentcam, image=self.bowlycama)
